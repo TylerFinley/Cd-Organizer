8ca2470cbb2f530dbc85cb3d1141fcdce201660d
leah klamkin tf tyler finley & tyler finley <student@epicodus.com>
2015-08-12 14:43:30 -0700

    Return the ID of the artist.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,14 @@ class Artist
 
     define_method(:initialize) do |name|
         @name = name
+        @id = @@all_artists.length().+(1)
     end
 
     define_method(:name) do
         @name
+    end
+
+    define_method(:id) do
+        @id
     end
 end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -13,4 +13,12 @@ describe(Artist) do
             expect(test_artist.name()).to(eq("Justin Bieber"))
         end
     end
+
+    describe('#id') do
+        it("returns the ID of the artist") do
+            test_artist = Artist.new("Justin Bieber")
+            expect(test_artist.id()).to(eq(1))
+        end
+    end
+
 end
