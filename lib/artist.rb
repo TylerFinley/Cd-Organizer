16b260fcb74ed4f32a1bcb4b2cea6559c1e334ff
leah klamkin tf tyler finley & tyler finley <student@epicodus.com>
2015-08-12 15:17:26 -0700

    Store albums for an artist and allow user to recall artist by its ID.

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,4 +22,28 @@ class Artist
     define_method(:save) do
         @@all_artists.push(self)
     end
+
+    define_singleton_method(:all) do
+        @@all_artists
+    end
+
+    define_singleton_method(:clear) do
+        @@all_artists = []
+    end
+
+    define_singleton_method(:find) do |id|
+        found_artist = nil
+        @@all_artists.each() do |artist|
+            if artist.id().eql?(id)
+                found_artist = artist
+            end
+        end
+        found_artist
+    end
+
+    define_method(:add_album) do |artist|
+        @albums.push(artist)
+    end
+
+
 end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -3,9 +3,9 @@ require('album')
 require('artist')
 
 describe(Artist) do
-    # before() do
-    #     Artist.clear()
-    # end
+    before() do
+        Artist.clear()
+    end
 
     describe('#name') do
         it("returns the name of the artist") do
@@ -32,6 +32,39 @@ describe(Artist) do
         it("adds an artist to the array of saved artists") do
             test_artist = Artist.new("Justin Bieber")
             expect(test_artist.save()).to(eq([test_artist]))
+        end
+    end
+
+    describe(".all") do
+        it("is empty at first") do
+            expect(Artist.all()).to(eq([]))
+        end
+    end
+
+    describe(".clear") do
+        it("empties out all of the saved artists") do
+            Artist.new("Justin Bieber").save()
+            Artist.clear()
+            expect(Artist.all()).to(eq([]))
+        end
+    end
+
+    describe(".find") do
+        it("returns a artist by its id number") do
+            test_artist = Artist.new("Justin Bieber")
+            test_artist.save()
+            test_artist2 = Artist.new("One Direction")
+            test_artist2.save()
+            expect(Artist.find(test_artist.id())).to(eq(test_artist))
+        end
+    end
+
+    describe('#add_album') do
+        it("adds a new album to an artist") do
+            test_artist = Artist.new("Justin Bieber")
+            test_album = Album.new("My World 2.0", 2010)
+            test_artist.add_album(test_album)
+            expect(test_artist.albums()).to(eq([test_album]))
         end
     end
 end
