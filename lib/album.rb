699222d781ba08bf5277a5aa865ed9645dae762c
leah klamkin tf tyler finley & tyler finley <student@epicodus.com>
2015-08-12 14:35:17 -0700

    Return the year the album was released.

--- lib/album.rb
+++ lib/album.rb
@@ -1,10 +1,16 @@
 class Album
     @@all_albums = []
 
-    define_method(:initialize) do |title|
+    define_method(:initialize) do |title, year|
         @title = title
+        @year = year
     end
     define_method(:title) do
         @title
     end
+    define_method(:year) do
+        @year
+    end
+
+
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -8,8 +8,14 @@ describe(Album) do
 
     describe("#title") do
         it("returns the title of the album") do
-            test_album = Album.new("My World 2.0")
+            test_album = Album.new("My World 2.0", 2010)
             expect(test_album.title()).to(eq("My World 2.0"))
+        end
+    end
+    describe("#year") do
+        it("returns the year the album was released") do
+            test_album = Album.new("My World 2.0", 2010)
+            expect(test_album.year()).to(eq(2010))
         end
     end
 end
